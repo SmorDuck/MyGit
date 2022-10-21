ce5b48c11f8eaad5dbf2d1cc4d0e8bfef5b8d8a9
SmorDuck <smorodin30579@mail.ru>
2022-10-22 00:32:10 +0300

    Add new class(Audi and Mazda)

---
+++ Audi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TestProject1
+{
+    class Audi : Car
+    {
+        private string equipment; //Комплектация
+
+        public Audi(string marka, string col, int prob, string equipment) : base(marka, col, prob)
+        {
+            this.equipment = equipment;
+            Print();
+            Razdel();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Марка нового автомобиля - " + Marka + ". Цвет авто - " + Collour + ". Пробег: " + Probeg + equipment);
+        }
+    }
+}
+
---
+++ Mazda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TestProject1
+{
+    class Mazda : Car
+    {
+        private string auto_pusk;
+
+        public Mazda(string marka, string collour, int probeg, string pusk) : base(marka, collour, probeg)
+        {
+            this.auto_pusk = pusk;
+            Print();
+            Razdel();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Марка автомобиля - " + Marka + ". Цвет авто - " + Collour + ". Пробег: " + Probeg + auto_pusk);
+        }
+    }
+}
+
