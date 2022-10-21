f4850b0fa2c71cfd48a69e64a2dcc6ce793d8ad3
SmorDuck <smorodin30579@mail.ru>
2022-10-22 00:54:40 +0300

    Add new class(Bike and Bus)

---
+++ Bike.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TestProject1
+{
+    public class Bike
+    {
+        public Bike()
+        {
+        }
+    }
+}
+
---
+++ Bus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TestProject1
+{
+    class Bus : Car
+    {
+        public Bus(string marka) : base(marka)
+        {
+            Print();
+            Razdel();
+        }
+
+         public override void Print()
+        {
+            Console.WriteLine("Когда машина в ремонте, я катаюсь на " + Marka);
+        }
+    }
+}
+
